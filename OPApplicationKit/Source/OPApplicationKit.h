6337decb340576bf5f170868a89f9db5f10065d5
Brandon Williams <brandon@kickstarter.com>
2012-01-18 11:59:03 -0500

    Added a header for including everything.

---
+++ OPApplicationKit/Source/OPApplicationKit.h
@@ -0,0 +1,13 @@
+//
+//  OPApplicationKit.h
+//  OPApplicationKit
+//
+//  Created by Brandon Williams on 1/18/12.
+//  Copyright (c) 2012 Opetopic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "OPAppDelegate.h"
+#import "OPApplication.h"
+#import "OPManagedObject.h"
